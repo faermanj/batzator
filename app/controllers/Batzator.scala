4938004153bad844e1c7183dbd905db43d09cdb6
Julio Faerman <jfaerman@gmail.com>
2013-03-10 15:18:38 -0300

    bootstrap only

--- app/controllers/Batzator.scala
+++ app/controllers/Batzator.scala
@@ -9,11 +9,10 @@ import play.api.mvc._
 
 
 object Batzator extends Controller {
-  val thingForm = Form(
-    "description" -> nonEmptyText)
+
 
   def index = Action { 
-    Ok(views.html.index(Thing.all,thingForm))
+    Ok(views.html.index(Thing.all))
   }
   
   def newThing = TODO 
